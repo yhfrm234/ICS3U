e945cda5d531836d19285ae0e592b460fe690e4c
323526723 <323526723@C-3ZWJYR1.tdsb.on.ca>
2016-05-09 14:30:12 -0400

    This if the if your happy and you know it progrma

---
+++ ICS3U/src/hagiali/HappyGame.java
@@ -0,0 +1,30 @@
+package hagiali;
+/* If your happy and you know it program
+ * It will show lyrics to if your happy and you know it
+ * May 2, 2016
+ * Ayub Hagi Ali
+ */
+
+
+public class HappyGame {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		//processing and outputs
+		String[] clap = new String []{"clap your hand", "stomp your feet" ," jump really high" , "touch your toes " , "raise your hands " , " sit down" , "nod your head" , "shake your arms " , "shake your legs " , "lie down" };
+		for (int i = 0; i <= 9; i++)
+		{
+			System.out.println(" if your happy and you know it, " + clap[i]);
+			System.out.println(" if your happy and you know it, " + clap[i]);
+			System.out.println(" if your happy and you know it, and you really wanna show it ");
+			System.out.println(" if your happy and you know it, " + clap[i]);
+		}
+		
+		
+		
+		
+	}
+
+}
