ed68dcd3b4df7fe9d6edb3b7ae052d911a24d0c6
323526723 <323526723@C-3ZWJYR1.tdsb.on.ca>
2016-05-09 14:28:58 -0400

    This is my list of names program

--- ICS3U/src/hagiali/ListOfNames.java
+++ ICS3U/src/hagiali/ListOfNames.java
@@ -1,5 +1,10 @@
 package hagiali;
 import java.util.Scanner;
+/* List of Names program
+ * It will list your names
+ * May 2, 2016
+ * Ayub Hagi Ali
+ */
 
 public class ListOfNames {
 
@@ -14,12 +19,15 @@ public class ListOfNames {
 		
 		int numberofnames;
 		
+		
+		//inputs
 		System.out.println("How much names do you want");
 		numberofnames = Integer.parseInt(scan.nextLine());
 		
 		String names [] = new String[numberofnames];
 		
 		System.out.println("Enter the " + numberofnames + " names that you want");
+		//processing and outputs
 		for (int i=0; i<= numberofnames - 1; i++)
 		{
 			names[i] = scan.nextLine();
