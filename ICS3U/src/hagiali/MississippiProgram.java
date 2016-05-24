ac68408019618893d8c344341c561eecbb74d41f
323526723 <323526723@C-3ZWJYR1.tdsb.on.ca>
2016-05-24 14:21:28 -0400

    This is my Mississippi Program

---
+++ ICS3U/src/hagiali/MississippiProgram.java
@@ -0,0 +1,92 @@
+package hagiali;
+
+
+
+public class MississippiProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		printM();
+		printI();
+		printS();
+		printS();
+		printI();
+		printS();
+		printS();
+		printI();
+		printP();
+		printP();
+		printI();
+		
+	}
+	
+	
+	public static void printM()
+	{
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+	}
+	
+	
+	
+	public static void printI()
+	{
+		System.out.println("IIIIIII");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("IIIIIII");
+	}
+	
+	
+	
+	public static void printS()
+	{
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+	}
+	
+	public static void printP()
+	{
+		System.out.println("PPPPPP ");
+		System.out.println("P     P");
+		System.out.println("P    P ");
+		System.out.println("P   P  ");
+		System.out.println("P  P   ");
+		System.out.println("PPP    ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+
+	
+
+}
